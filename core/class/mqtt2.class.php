468ee9d66b8f9e52a70c463fb295666ca598cc0d
Loïc <1536036+zoic21@users.noreply.github.com>
2024-08-15 18:06:47 +0200

    bugfix

--- core/class/mqtt2.class.php
+++ core/class/mqtt2.class.php
@@ -1300,7 +1300,7 @@ class mqtt2 extends eqLogic {
          if (file_exists(__DIR__ . '/../../../../' . $file)) {
             return $file;
          }
-         return 'plugins/mqtt2/core/config/devices/jeedom/mqtt2.png';
+         return 'plugins/mqtt2/core/config/devices/jeedom/mqtt.png';
       }
       $file = 'plugins/mqtt2/core/config/devices/' . self::getImgFilePath($this->getConfiguration('device'));
       if (!file_exists(__DIR__ . '/../../../../' . $file)) {
