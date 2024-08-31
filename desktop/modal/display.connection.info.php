dda8f3ab4016972f47b07b5e1277512a6241a45c
Loïc <1536036+zoic21@users.noreply.github.com>
2024-08-31 14:45:04 +0200

    bugfix

--- desktop/modal/display.connection.info.php
+++ desktop/modal/display.connection.info.php
@@ -20,7 +20,7 @@ if (!isConnect('admin')) {
 $local_authentifications = explode(':', explode("\n", config::byKey('mqtt::password', __CLASS__))[0]);
 $connection_info = array(
   'topic' => config::byKey('root_topic', 'mqtt2'),
-  'id' => substr(jeedom::getHardwareKey(),0,10)
+  'id' => substr(jeedom::getHardwareKey(),0,10),
   'ip' => network::getNetworkAccess('inernal','ip'),
   'port' => 1833,
   'username' => $local_authentifications[0],
